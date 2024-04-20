0a34832f2fe19821e1d501212355b501ecb41140
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-20 08:25:00 -0300

    wip: implement class linked list

---
+++ 03_linked-list/linked-list.js
@@ -0,0 +1,41 @@
+import { Node } from "../models";
+import { defaultEquals } from "../util";
+
+class LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    this.count = 0;
+    this.head = undefined;
+    this.equalsFn = equalsFn;
+  }
+
+  push(element) {
+    const node = new Node(element);
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.count++;
+  }
+
+  removeAt(index) {
+    if (index < 0 || index >= this.count) return undefined;
+    let current = this.head;
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      let previous;
+      for (let i = 0; i < index; i++) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+    }
+    this.count--;
+    return current.element;
+  }
+}
