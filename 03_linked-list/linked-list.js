7e9c981efa06821e805e79a23dab3890b1555e7b
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-21 07:50:46 -0300

    implement class linked list

--- 03_linked-list/linked-list.js
+++ 03_linked-list/linked-list.js
@@ -44,4 +44,58 @@ class LinkedList {
     }
     return node;
   }
+
+  insert(element, index) {
+    if (index < 0 || index > this.count) return false;
+    const node = new Node(element);
+    let current = this.head;
+    if (index === 0) {
+      node.next = current;
+      this.head = node;
+    } else {
+      const previous = this.getElementAt(index - 1);
+      current = previous.next;
+      node.next = current;
+      previous.next = node;
+    }
+    this.count++;
+    return true;
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    for (let i = 0; i < this.count && current != null; i++) {
+      if (this.equalsFn(element, current.element)) return i;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  remove(element) {
+    const index = this.indexOf(element);
+    return this.removeAt(index);
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  toString() {
+    if (this.isEmpty()) return "";
+    let objString = `${this.head.element}`;
+    let current = this.head.next;
+    for (let i = 1; i < this.size() && current != null; i++) {
+      objString += `,${current.element}`;
+      current = current.next;
+    }
+    return objString;
+  }
 }
