3af1a348a50089d6e593effad70a4b78f9323e58
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-23 10:12:26 -0300

    implement ValuePair model

--- models.js
+++ models.js
@@ -5,4 +5,15 @@ class Node {
   }
 }
 
-module.exports = { Node };
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
+}
+
+module.exports = { Node, ValuePair };
