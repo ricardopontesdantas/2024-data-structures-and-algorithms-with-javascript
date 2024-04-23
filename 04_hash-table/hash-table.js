6ae0c2ec0fc1567bf523b66ed334d199a92b3561
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-23 10:13:32 -0300

    implement class HashTable

--- 04_hash-table/hash-table.js
+++ 04_hash-table/hash-table.js
@@ -1,3 +1,4 @@
+const { ValuePair } = require("../models");
 const { defaultToString } = require("../util");
 
 class HashTable {
@@ -16,7 +17,30 @@ class HashTable {
     return hash % 37;
   }
 
-  hashCodeKey(key) {
+  hashCode(key) {
     return this.loseloseHashCode(key);
   }
+
+  put(key, value) {
+    if (key == null || value == null) return false;
+    const position = this.hashCode(key);
+    this.table[position] = new ValuePair(key, value);
+    return true;
+  }
+
+  get(key) {
+    const hash = this.hashCode(key);
+    const valuePair = this.table[hash];
+    return valuePair == null ? undefined : valuePair.value;
+  }
+
+  remove(key) {
+    const hash = this.hashCode(key);
+    const valuePair = this.table[hash];
+    if (valuePair != null) {
+      return delete this.table[this.hashCode(key)];
+    }
+    return false;
+  }
 }
+
