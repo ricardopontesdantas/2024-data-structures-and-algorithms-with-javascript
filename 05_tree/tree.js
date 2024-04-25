3a199767ac653dbad9f7dc5c2627914ec0990b65
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-25 07:41:08 -0300

    wip: implement class BinarySearchTree
    Implement inOrderTraverse, preOrderTraverse and postOrderTraverse methods

--- 05_tree/tree.js
+++ 05_tree/tree.js
@@ -30,4 +30,38 @@ class BinarySearchTree {
       }
     }
   }
+
+  inOrderTraverse(callback) {
+    this.inOrderTraverseNode(this.root, callback);
+  }
+
+  inOrderTraverseNode(node, callback) {
+    if (node != null) {
+      this.inOrderTraverseNode(node.left, callback);
+      callback(node.key);
+      this.inOrderTraverseNode(node.right, callback);
+    }
+  }
+
+  preOrderTraverse(callback) {
+    this.preOrderTraverseNode(this.root, callback);
+  }
+
+  preOrderTraverseNode(node, callback) {
+    if (node != null) {
+      callback(node.key);
+      this.preOrderTraverseNode(node.left, callback);
+      this.preOrderTraverseNode(node.right, callback);
+    }
+  }
+
+  postOrderTraverse(callback) {
+    this.postOrderTraverseNode(this.root, callback);
+  }
+
+  postOrderTraverseNode(node, callback) {
+    this.postOrderTraverseNode(node.left, callback);
+    this.postOrderTraverseNode(node.right, callback);
+    callback(node.key);
+  }
 }
