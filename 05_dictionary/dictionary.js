336ad7ce192c6beb6fd7c2f78336a6db7ea213b4
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-29 19:09:18 -0300

    implement class Dictionary

--- 05_dictionary/dictionary.js
+++ 05_dictionary/dictionary.js
@@ -28,4 +28,46 @@ class Dictionary {
     const valuePair = this.table[this.toStrFn(key)];
     return valuePair == null ? undefined : valuePair.value;
   }
+
+  keyValues() {
+    return Object.values(this.table);
+  }
+
+  keys() {
+    return this.keyValues().map((valuePair) => valuePair.key);
+  }
+
+  values() {
+    return this.keyValues().map((valuePair) => valuePair.value);
+  }
+
+  forEach(callback) {
+    const valuePairs = this.keyValues();
+    for (let i = 0; i < valuePairs.length; i++) {
+      const result = callback(valuePairs[i].key, valuePairs[i].value);
+      if (result == false) break;
+    }
+  }
+
+  size() {
+    return this.keyValues().length;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  clear() {
+    this.table = {};
+  }
+
+  toString() {
+    if (this.isEmpty()) return "";
+    const valuePairs = this.keyValues();
+    let objString = `${valuePairs[0].toString()}`;
+    for (let i = 1; i < valuePairs.length; i++) {
+      objString += `${valuePairs[i].toString()}`;
+    }
+    return objString;
+  }
 }
