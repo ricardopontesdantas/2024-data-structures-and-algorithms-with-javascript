4ebd38c1ca129d83973ce64971133cb3c48a2b5e
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-05-29 08:27:02 -0300

    implement depthFirstSearch function
    Depth-First Search (DFS) algorithm

--- search-algorithms.js
+++ search-algorithms.js
@@ -23,4 +23,28 @@ const breadthFirstSearch = (graph, startVertex, callback) => {
   }
 };
 
-module.exports = { breadthFirstSearch };
+const depthFirstSearch = (graph, callback) => {
+  const vertices = graph.getVertices();
+  const adjList = graph.getAdjList();
+  const color = initializeColor(vertices);
+  for (let i = 0; i < vertices.length; i++) {
+    if (color[vertices[i]] === Colors.WHITE) {
+      depthFirstSearchVisit(vertices[i], color, adjList, callback);
+    }
+  }
+};
+
+const depthFirstSearchVisit = (u, color, adjList, callback) => {
+  color[u] = Colors.GRAY;
+  if (callback) callback(u);
+  const neighbors = adjList.get(u);
+  for (let i = 0; i < neighbors.length; i++) {
+    const w = neighbors[i];
+    if (color[w] === Colors.WHITE) {
+      depthFirstSearchVisit(w, color, adjList, callback);
+    }
+  }
+  color[u] = Colors.BLACK;
+};
+
+module.exports = { breadthFirstSearch, depthFirstSearch };
