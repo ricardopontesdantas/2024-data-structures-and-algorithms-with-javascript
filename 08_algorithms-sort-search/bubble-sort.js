5325a2df5114191a9137109e8759a17bd817e65d
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-06-01 14:24:52 -0300

    implement bubble sort algorithm

---
+++ 08_algorithms-sort-search/bubble-sort.js
@@ -0,0 +1,23 @@
+const {
+  defaultCompare,
+  Compare,
+  swap,
+  createNonSortedArray,
+} = require("../util");
+
+function bubbleSort(array, compareFn = defaultCompare) {
+  const { length } = array;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1; j++) {
+      if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+        swap(array, j, j + 1);
+      }
+    }
+  }
+  return array;
+}
+
+const array = createNonSortedArray(10);
+console.log(array.join());
+bubbleSort(array);
+console.log(array.join());
