76d9825ff3735181494087ce2fb7df209ee14573
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-06-03 07:23:49 -0300

    implement insertion sort algorithm

---
+++ 08_algorithms-sort-search/insertion-sort.js
@@ -0,0 +1,25 @@
+const {
+  defaultCompare,
+  Compare,
+  swap,
+  createNonSortedArray,
+} = require("../util");
+
+function insertionSort(array, compareFn = defaultCompare) {
+  const { length } = array;
+  let temp;
+  for (let i = 1; i < length; i++) {
+    let j = i;
+    temp = array[i];
+    while (j > 0 && compareFn(array[j - 1], temp) === Compare.BIGGER_THAN) {
+      array[j] = array[j - 1];
+      j--;
+    }
+    array[j] = temp;
+  }
+}
+
+const array = createNonSortedArray(10);
+console.log(array.join());
+insertionSort(array);
+console.log(array.join());
