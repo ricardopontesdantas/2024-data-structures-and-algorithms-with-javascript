6b8b9d502d06f332885af2714d8b4491a59ca51a
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-06-11 07:10:11 -0300

    fix sequential search

--- 08_algorithms-sort-search/sequential-search.js
+++ 08_algorithms-sort-search/sequential-search.js
@@ -5,6 +5,9 @@ const DOES_NOT_EXIST = -1;
 function sequentialSearch(array, value, equalsFn = defaultEquals) {
   for (let i = 0; i < array.length; i++) {
     if (equalsFn(value, array[i])) return i;
-    return DOES_NOT_EXIST;
   }
+  return DOES_NOT_EXIST;
 }
+
+array = [1, 2, 3, 4, 5];
+console.log(sequentialSearch(array, 3));
